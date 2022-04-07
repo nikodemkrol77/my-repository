fb4e0de267ed7b7f3e0dbd5e59a9c494aeb2d1db
nikodemkrol77 <99958825+nikodemkrol77@users.noreply.github.com>
2022-04-07 19:50:00 +0200

    Add files via upload

---
+++ NWD/NWD.cpp
@@ -0,0 +1,27 @@
+﻿// NWD.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while (a != b)
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    return a;
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Podaj dwie liczby: ";
+    cin >> a >> b;
+
+    cout << "NWD(" << a << "," << b << ") = " << NWD(a, b) << endl;
+
+    return 0;
+}
---
+++ NWW/NWW.cpp
@@ -0,0 +1,33 @@
+﻿// NWW.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+	
+#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+	int pom;
+
+	while (b != 0)
+	{
+		pom = b;
+		b = a % b;
+		a = pom;
+	}
+	return a;
+}
+
+int main()
+{
+	unsigned int a, b;
+
+	cout << "Podaj dwie liczby: ";
+	cin >> a >> b;
+
+	cout << "NWW(" << a << ", " << b << ") = " << a / NWD(a, b) * b << endl;
+
+	cin.ignore();
+	cin.get();
+
+	return 0;
+
